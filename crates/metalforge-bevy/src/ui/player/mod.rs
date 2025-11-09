189ff8e44eeea4632b2e2afa6d178fb2b11295e5
Alex Pecsi <alex.pecsi@gmail.com>
2025-11-09 10:58:43 +0000

    Work on camera movement in progress

--- crates/metalforge-bevy/src/ui/player/mod.rs
+++ crates/metalforge-bevy/src/ui/player/mod.rs
@@ -1,9 +1,15 @@
+use std::time::Duration;
 use crate::ui::AppState;
-use bevy::app::App;
+use bevy::app::{App, FixedUpdate, Update};
 use bevy::asset::{AssetServer, Handle};
-use bevy::prelude::{default, AppExtStates, Bundle, Color, Commands, DespawnOnExit, Message, OnEnter, Res, States, Text};
+use bevy::camera::Camera2d;
+use bevy::math::Vec2;
+use bevy::prelude::{default, in_state, AppExtStates, Bundle, Color, Commands, DespawnOnExit, IntoScheduleConfigs, Message, OnEnter, Query, Res, ResMut, Resource, States, SystemCondition, Text, Transform, With};
+use bevy::sprite::Sprite;
 use bevy::text::{Font, TextColor, TextFont};
+use bevy::time::Time;
 use bevy::ui::{percent, AlignItems, BackgroundColor, JustifyContent, Node};
+use bevy::ui::Val::Px;
 
 #[derive(Message, Copy, Clone)]
 pub enum PlayerEvent {
@@ -18,20 +24,58 @@ pub enum PlayerState {
     Paused,
 }
 
+#[derive(Resource, Default)]
+pub struct Player {
+    song_position: Duration
+}
+
 pub fn player_plugin(app: &mut App) {
     app
         .insert_state(PlayerState::Playing)
+        .insert_resource(Player::default())
         .add_message::<PlayerEvent>()
-        .add_systems(OnEnter(AppState::Player), setup_player);
+        .add_systems(OnEnter(AppState::Player), setup_player)
+        .add_systems(FixedUpdate, update_player.run_if(in_state(AppState::Player)))
+        .add_systems(Update, move_camera.run_if(in_state(AppState::Player).and(in_state(PlayerState::Playing))));
+}
+
+fn update_player(mut player: ResMut<Player>, time: Res<Time>) {
+    player.song_position = time.elapsed();
+}
+
+fn move_camera(mut query: Query<&mut Transform, With<Camera2d>>, player: Res<Player>) {
+    let Ok(mut camera) = query.single_mut() else {
+        return;
+    };
+
+    camera.translation.x = player.song_position.as_millis() as f32 / 10.0;
+
+    /*
+    if player_state.song_playing {
+        // camera.translation.x += 1.0;
+    }
+     */
 }
 
 /// Initialise the tab player screen
 fn setup_player(mut commands: Commands,  asset_server: Res<AssetServer>) {
     // Prepare assets
-    let font = asset_server.load("fonts/LelandText.otf");
+    // - fonts can be loaded using asset_server.load("fonts/LelandText.otf");
 
     // Draw the individual notes
+    commands.spawn(Cursor::new());
 
+    /*
+    for i in 0..10 {
+        commands.spawn((Node {
+            width: Px(30.0),
+            height: Px(30.0),
+            ..default()
+        }, Transform::from_xyz(i as f32 * 12.0, i as f32 * 15.0, 0.0), BackgroundColor(Color::srgb(0.8, 0.3, 0.3))));
+    }
+     */
+
+    /*
     commands.spawn((
         DespawnOnExit(AppState::Player),
         BackgroundColor(Color::srgb(0.3, 0.3, 0.3)),
@@ -46,6 +90,22 @@ fn setup_player(mut commands: Commands,  asset_server: Res<AssetServer>) {
     )).with_children(|parent| {
         parent.spawn(TitleText::new(font.clone()));
     });
+     */
+}
+
+#[derive(Bundle)]
+struct Cursor {
+    sprite: Sprite,
+    transform: Transform
+}
+
+impl Cursor {
+    pub fn new() -> Self {
+        Self {
+            sprite: Sprite::from_color(Color::srgb(1.0, 1.0, 1.0), Vec2::new(1.0, 60.0)),
+            transform: Transform::from_xyz(0.0, 0.0, 1.0),
+        }
+    }
 }
 
 #[derive(Bundle)]
